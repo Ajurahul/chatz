d72773d0c3fa190ce25cf0d42a2cedddc533f48a
Aju <ajurahul02@gmail.com>
2019-03-22 23:41:09 +0530

    awesome works

--- app/src/main/java/com/mysocial/chatz/Login.java
+++ app/src/main/java/com/mysocial/chatz/Login.java
@@ -2,28 +2,19 @@ package com.mysocial.chatz;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
     TextView registerUser;
@@ -71,6 +62,7 @@ public class Login extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
                                 finish();
+UserDetails.username=user;
                                 startActivity(new Intent(getApplicationContext(),Chat.class));
                             }
                             else{
--- app/src/main/java/com/mysocial/chatz/Register.java
+++ app/src/main/java/com/mysocial/chatz/Register.java
@@ -2,35 +2,22 @@ package com.mysocial.chatz;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.firebase.client.Firebase;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 public class Register extends AppCompatActivity {
 
-    EditText username, password,usern;
+    EditText username, password;
     Button registerButton;
-    String user, pass,userna;
+    String user, pass;
     TextView login;
     Firebase reference1;
     private FirebaseAuth firebaseAuth;
@@ -39,14 +26,14 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         firebaseAuth = FirebaseAuth.getInstance();
-        usern = (EditText)findViewById(R.id.usern);
+
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
         registerButton = (Button)findViewById(R.id.registerButton);
         login = (TextView)findViewById(R.id.login);
 
-        Firebase.setAndroidContext(this);
-        reference1 = new Firebase("https://chat-33452.firebaseio.com/users");
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,7 +46,7 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 user = username.getText().toString().trim();
                 pass = password.getText().toString().trim();
-                userna = usern.getText().toString().trim();
+
                 if(user.equals("")){
                     username.setError("can't be blank");
                 }
@@ -77,8 +64,8 @@ public class Register extends AppCompatActivity {
                     firebaseAuth.createUserWithEmailAndPassword(user, pass);
 
                     pd.dismiss();
-             UserDetails.username = userna;
-                    reference1.child(userna).child("password").setValue(pass);
+
+
 
                     Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
 
